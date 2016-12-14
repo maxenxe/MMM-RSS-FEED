078c91a6a65968e05ac96333e01817d52f3a1380
Taolanoz <maxence.renglet@gmail.com>
2016-12-14 18:25:50 +0100

    Add files via upload

---
+++ MMM-RSS-FEED.js
@@ -0,0 +1,34 @@
+Module.register("MMM-RSS-FEED",{
+	
+	defaults: {
+		url: ""
+	},
+	getScripts: function() {
+		return ["modules/MMM-RSS-FEED/js/jquery-3.1.1.min.js","modules/MMM-RSS-FEED/js/feednami.js"];
+	},
+		getStyles: function() {
+		return ["modules/MMM-RSS-FEED/css/style.css"];
+	},
+	start: function() {
+		Log.info("Starting module: " + this.name);
+	},
+	
+	getDom: function () {
+		var wrapper = document.createElement("div");
+    		var url = this.config.url;
+		feednami.load(url,function(result){
+			if(result.error) {
+				console.log(result.error);
+			} else {
+				var entries = result.feed.entries;
+				for(var i = 0; i < entries.length; i++){
+				var entry = entries[i];
+				console.dir(entry);
+				console.log(entry);
+				wrapper.innerHTML = entry.title;
+				}
+			}
+		});
+		return wrapper;
+	}
+});
